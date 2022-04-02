6823a8770009af47ecb214729ba16d2282daac68
longvc <longvc@hybrid-technologies.vn>
2022-04-02 23:53:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+   export default {
+    data() {
+        return {
+            name: "Long"
+        }
+    },
+    methods: {
+        onChangeName: function() {
+            console.log('say hello')
+        }
+    },
+    mounted() {
+        console.log(this)
+    }
+}
+</script>
